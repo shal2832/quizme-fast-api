1fca737d99ecd41cce31a5b290feb304cd8feb20
vaseekaran002 <vasee002@gmail.com>
2026-02-28 19:56:51 +0530

    feat(api): file upload api endpoint

--- src/service/processFile.py
+++ src/service/processFile.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+
 from fastapi.responses import JSONResponse
 
-router = APIRouter(prefix="", tags=["chat"])
+def chunk_file(file, chunk_size):
+    """
+    Generator that yields chunks of the file.
+    
+    :param file: The file to be chunked.
+    :param chunk_size: The size of each chunk in bytes.
+    """
+    # while True:
+    #     chunk = file.read(chunk_size)
+    #     if not chunk:
+    #         break
+    #     yield chunk
 
-@router.post('/chat')
-def chat(message: str):
-    return JSONResponse(content={"Your message": message}, status_code=200)
+    return JSONResponse(content={"message": "File processed successfully", "file" : file.filename}, status_code=200)
