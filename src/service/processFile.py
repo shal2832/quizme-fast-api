27970cab822d5def69f6c9b3196bd91d3ad4a4ff
Shalinie <shalinie@Shalinies-MacBook-Air.local>
2026-03-11 22:35:00 +0530

    added logs for api calls

--- src/service/processFile.py
+++ src/service/processFile.py
@@ -18,12 +18,16 @@ def chunk_file(content, filename):
     for doc in documents:
         doc.metadata["file_id"] = filename
         document_content.append({"page_content": doc.page_content, "metadata": doc.metadata})
+    print("Calling split documents API with document content")
     docs = qdrantApiServiceInstance.split_documents_api(document_content)
+    print(f"Split documents API response received with status code: {docs.status_code}")
     # for passing in api response only
     serializable_chunks = [{"page_content": doc.page_content, "metadata": doc.metadata} for doc in docs.json().get("documents", [])
     ]
 
     #pass the file name inside qdrant service, so that it can be used when retrieving context
     ### qdrant_service_instance.set_file_name(filename)
-    qdrantApiServiceInstance.add_documents_api(docs)
+    print("Calling add documents API with split documents")
+    response = qdrantApiServiceInstance.add_documents_api(docs)
+    print(f"Added documents API response received with status code: {response.status_code}")
     return temp_file_path, serializable_chunks
